7ff5607b4a68c1803ccd30d5e46bd3b3439e6a56
rmrfbot2 <tylacy@my.lonestar.edu>
2025-10-08 22:58:39 -0500

    Create movement.cpp
    made a basic skeleton for us to start building off of

---
+++ code/movement.cpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////
+////          title: vex small robot navigation             ////
+////          author: tyler                                 ////
+////          description: code for our robot's navigation; ////
+////          automation, block detection, etc.             ////
+////////////////////////////////////////////////////////////////              
+
+#include <cmath>
+
+int main()
+{
+  //move forward
+    void moveWheelsForward() {
+      rightMotorForward();
+      leftMotorForward();
+    }
+  //move backward
+    void moveWheelsBackward() {
+      rightMotorBackward();
+      leftMotorBackward();
+    }
+  //spin left
+  //spin right
+return 0;
+}
+  
